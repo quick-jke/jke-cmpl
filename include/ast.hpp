07e6c5f57e9d14a3190c5cd95c236370ca117be9
DeadEndjke <romainobodin@yandex.ru>
2025-06-05 17:26:25 +0700

    [added] some methods

--- include/ast.hpp
+++ include/ast.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <memory>
 #include <sstream>
+#include <map>
 namespace quick {
 namespace jkecmpl{
 enum class FieldType {
@@ -64,7 +65,6 @@ struct Field {
                           type_ == FieldType::Bool ? "bool " :
                           type_ == FieldType::Char ? "char " :
                           "custom:" + custom_type_)
-            << "get_" 
             << name_ 
             << "(){return " 
             << name_ << "_;}";
@@ -107,6 +107,111 @@ struct Field {
         return oss.str();
     }
 
+    std::string expr(){
+        //int, float, decimal, bigint
+        // > < >= <= ==
+        // between and
+        // in 
+        // ariphmetic
+        
+        //strings
+        // == !=
+        // like
+        // in
+
+        //date datetime timestamp
+        // == > < <= >=
+        // between and
+        // year month day 
+
+        //boolean
+        // true false
+
+        // null
+        std::stringstream oss;
+
+        std::map<std::string, std::string> int_exprs = {
+            {"_more_than", " > "},        // a > 5                           
+            {"_less_than", " < "},        // a < 5                    
+            {"_more_or_equal", " >= "},    // a >=5                           
+            {"_less_or_equal", " <= "},    // a <=5                           
+            {"_equal", " = "},             // a = 5                      
+            {"_not_equal", " != "}          // a !=5     
+            // a between 5 and 10
+            // in (1, 2, 3)                    
+        };
+
+       
+
+        std::map<std::string, std::string> str_exprs = {
+            {"_equal", " = \" + "},             // str = 'str'
+            {"_not_equal", " != \" + "},         // str !='str'
+            {"_like", " LIKE \" + "},           // str LIKE 'str'
+            // str IN ('val1', 'val2')
+        };
+
+        std::map<std::string, std::string> bool_exprs = {
+            {"_true", " IS TRUE"},
+            {"_false", " IS FALSE"}
+        };
+
+        std::map<std::string, std::string> null_exprs = {
+            {"_is_null", " IS NULL"},
+            {"_is_not_null", " IS NOT NULL"}
+        };
+
+
+        if(type_ == FieldType::Int || type_ == FieldType::Double){
+            for(auto pair : int_exprs){
+                oss << "\tstd::string " << name_ << pair.first << "(" 
+                << (type_ == FieldType::Int ? "int " :
+                    type_ == FieldType::Double ? "double " :
+                    type_ == FieldType::Bool ? "bool " :
+                    type_ == FieldType::Char ? "char " :
+                    "custom: " + custom_type_)
+                << "value){ return \"" << name_ << pair.second << "\" + " << "std::to_string(value);}" << std::endl;
+            }
+            oss << "\tstd::string " << name_ << "_between_and(int val1, int val2){ return \"" << name_ << " BETWEEN\" + " << "std::to_string(val1)" << " + \" AND \" + " << "std::to_string(val2);}" << std::endl;
+            
+            oss << "\tstd::string " << name_ << "_in(std::vector<int> values){" << std::endl;
+            oss << "std::stringstream oss;" << std::endl;
+            oss << "oss << \"(\";" << std::endl;
+            oss << "for(size_t i = 0; i < values.size(); ++i){" << std::endl;
+            oss << "\t\toss << values.at(i);" << std::endl;
+            oss << "\t\tif(i != values.size() - 1){" << std::endl;
+            oss << "\t\t\t\toss << \", \";" << std::endl;
+            oss << "\t\t}" << std::endl;
+            oss << "}" << std::endl;
+            oss << "oss << \")\";" << std::endl;
+            oss << "return \"rating in \" + oss.str();" << std::endl
+                << "\t}" 
+                << std::endl; 
+        }else if(type_ == FieldType::String){
+            for(auto pair : str_exprs){
+                oss << "\tstd::string " << name_ << pair.first 
+                << "(const std::string& str_val){"
+                << "return \"" << name_ << pair.second << " str_val;}" 
+                << std::endl;
+            }
+        }else if(type_ == FieldType::Bool){
+            for(auto pair : bool_exprs){
+                oss << "\tstd::string " << name_ << pair.first
+                << "(){ return \"" << name_ << pair.second << "\";}"
+                << std::endl;
+            }
+        }
+
+        for(auto pair : null_exprs){
+            oss << "\tstd::string " << name_ << pair.first
+                << "(){ return \"" << name_ << pair.second << "\";}"
+                << std::endl;
+        }
+
+        
+
+        return oss.str();
+    }
+
 };
 
 enum class RelationType {
@@ -128,10 +233,12 @@ struct Relation {
         
         if(type_ == RelationType::OneToOne){
             oss << "\t" << target_table_ << " " << field_name_ << "; // one to one" << std::endl;
-        }else if(type_ == RelationType::ManyToMany || type_ == RelationType::OneToMany){
+        }else if(type_ == RelationType::ManyToMany){
             oss << "\tstd::vector<" << target_table_ << "> " << field_name_ << "_; //" 
             << (type_ == RelationType::ManyToMany ? "many to many" : "one to many")
             << std::endl;
+        }else if(type_ == RelationType::ManyToOne){
+            oss << "\t" << target_table_ << " " << field_name_ << "_id;" << std::endl;
         }
         else{
             oss << "\t//" << field_name_ << " -> " << target_table_ << " ("
@@ -178,7 +285,7 @@ struct Table {
         oss << "\t" << name_ << "(){}" << std::endl;
 
         //namefunc
-        oss << "\tstd::string name(){return \"" << name_ << "\";}" << std::endl; 
+        oss << "\tstd::string table_name(){return \"" << name_ << "\";}" << std::endl; 
 
         //columns 
         oss << "\tstd::vector<Column> columns(){" << std::endl;
@@ -212,7 +319,7 @@ struct Table {
 
         }
         for(size_t i = 0; i < relations_.size(); ++i){
-            if(relations_.at(i)->type_ != RelationType::ManyToMany){
+            if(relations_.at(i)->type_ != RelationType::ManyToMany ){
                 oss << "\"" << relations_.at(i)->field_name_ << "_id\"";
                 if(i != relations_.size() - 1){
                     oss << ", ";
@@ -227,13 +334,22 @@ struct Table {
             oss << field->setter() << std::endl;
             oss << field->getter() << std::endl;
         }
+
+        //expressions
+        for(auto field : fields_){
+            oss << field->expr() << std::endl;
+        }
+
+
+
         //private fields
         oss << "\nprivate:" << std::endl;
         for(auto field : fields_){
             oss << field->to_string();
         }
         //relations
-        oss << "//Relations:\n";
+        relations_.size() && oss << "//Relations:\n";
+        
         for(auto rel : relations_){
             oss << rel->to_string();
         }
